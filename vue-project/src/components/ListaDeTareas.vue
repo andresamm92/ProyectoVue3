78312acc6cd5a772ede3db7a75c73e91b5158c99
andresamm <andresalbertomm92@outlook.com>
2024-11-01 10:35:15 -0600

    Crea componente "Lista de tareas", borra componentes autogenerados y crea rutas.

---
+++ vue-project/src/components/ListaDeTareas.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" name="" id="" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(itemTarea, index) in tareas" :key="index">
+                {{ itemTarea }}
+                <button @click="borrarTarea">Borrar Tarea</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const tareas = ref([]);
+const nuevaTarea = ref('');
+
+const addTarea = () => {
+    tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value = '';
+}
+
+const borrarTarea = (index) => {
+    tareas.value.splice(index, 1);
+}
+</script>
+
+<style>
+</style>
